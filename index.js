99b1ea3aaf6ebeee71cf91911f9c90b946bc1abe
el1ght <el1ghtifyoucan@gmail.com>
2024-10-05 21:31:15 +0300

    add web app url

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const TelegramBot = require('node-telegram-bot-api')
 
 const token = '';
+const webAppUrl = '';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
+    const text = msg.text;
 
-    bot.sendMessage(chatId, 'Received your message');
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'button below', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Form', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
 })
